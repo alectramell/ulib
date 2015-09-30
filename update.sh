010565619bf3b76c82be58ec87b2863af8ddb69f
alectramell <alectramell@gmail.com>
2015-09-30 15:37:32 -0700

    UMBRA

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ USERNAME=$(whoami)
 
 clear
 
-echo "Umbra update is active, please do not close this window until finishe!" && sleep 7 && clear
+echo "Umbra update is active, please do not close this window until finished!" && sleep 7 && clear
 
 sudo apt-get update
 
@@ -14,15 +14,9 @@ sudo apt-get install git
 
 clear
 
-sudo rm -r ~/ulib
-
-sudo rm -r /home/$USERNAME/Desktop/ulib
-
-clear
-
 cd /home/$USERNAME/Desktop/ && git clone https://github.com/alectramell/ulib.git && clear
 
 clear
 
-sudo dpkg -i /home/$USERNAME/Desktop/ulib/umbra-1.0.deb && zenity --info --title="Umbra v1.0" --text="Update Complete!" && clear
+sudo dpkg -i /home/$USERNAME/Desktop/ulib/umbra-1.0.deb && zenity --info --title="Umbra v1.0" --text="Update Complete!" && clear && sudo rm -r ~/ulib && sudo rm -r /home/$USERNAME/Desktop/ulib
 
